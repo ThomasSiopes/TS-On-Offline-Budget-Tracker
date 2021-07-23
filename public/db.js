5642c36b955732afc0073e05e5d41d697ad0ba4a
theeshyguy <t.siopes00@yahoo.com>
2021-07-23 00:20:52 -0400

    minor syntax and spelling changes

--- public/db.js
+++ public/db.js
@@ -6,7 +6,7 @@ const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
 
-  const { oldVersion } = e;
+  const oldVersion = e;
   const newVersion = e.newVersion || db.version;
 
   console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
